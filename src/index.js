d30c4c64fcbaeb6a8da4e7b19c18d8a5fcc5a25d
viggoStrom <149628098+ClassyVenator@users.noreply.github.com>
2023-11-03 18:43:31 +0100

    reverse engineering bmp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var tf = require("@tensorflow/tfjs");
-var list = tf.randomNormal([1000]);
-console.log(tf.argMax(list).dataSync()[0]);
+var fs = require("fs");
+// const listLength: number = 1000
+// const list: tf.Tensor = tf.randomNormal([listLength])
+// const max: number = tf.argMax(list).dataSync()[0]
+// const min: number = tf.argMin(list).dataSync()[0]
+// const span: number = max - min
+// const values: number[] = new Array(40)
+// for (let index = 0; index < listLength; index++) {
+// if () {
+// }
+// }
+// const map: string[] = [ // ▮
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+// ]
+var addLeadingZeros = function (binaryString) {
+    if (binaryString === "0") {
+        return "00000000";
+    }
+    else {
+        return binaryString.replace(/^0*/, "0".repeat(8 - binaryString.length));
+    }
+};
+var bitmap = fs.readFileSync("./graphs/normalDist.bmp", "binary").split("");
+var bitmapLength = bitmap.length;
+for (var index = 0; index < bitmapLength; index++) {
+    var bin = bitmap[index].charCodeAt(0).toString(2);
+    if (bin !== "11111111") {
+        var row = "".concat(index, " ").concat(bitmap[index], " ").concat(addLeadingZeros(bin));
+        console.log(row);
+    }
+    // fs.appendFileSync("./outputs/binOut.txt", addLeadingZeros(bin) + "\n", "binary")
+}
+// const fileRows = fs.readFileSync("./outputs/binOut.txt", "binary").split("\n")
+// fileRows.forEach((row, index) => {
+//     const [base, middle, topLeft, bottomRight] = row.split("  ")
+//     if (base !== bottomRight) {
+//         console.log(index, row);
+//     }
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,61 @@
 
 import * as tf from "@tensorflow/tfjs"
-import fs from "fs"
+import * as fs from "fs"
 
-const list: tf.Tensor = tf.randomNormal([1000])
+// const listLength: number = 1000
+// const list: tf.Tensor = tf.randomNormal([listLength])
+// const max: number = tf.argMax(list).dataSync()[0]
+// const min: number = tf.argMin(list).dataSync()[0]
+// const span: number = max - min
+// const values: number[] = new Array(40)
 
-console.log(tf.argMax(list).dataSync()[0]);
+// for (let index = 0; index < listLength; index++) {
+// if () {
+
+// }
+// }
+
+// const map: string[] = [ // ▮
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+//     "▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯▯",
+// ]
+
+const addLeadingZeros = (binaryString: string): string => {
+    if (binaryString === "0") {
+        return "00000000"
+    } else {
+        return binaryString.replace(/^0*/, "0".repeat(8 - binaryString.length));
+    }
+}
+
+const bitmap: string[] = fs.readFileSync("./graphs/normalDist.bmp", "binary").split("")
+const bitmapLength: number = bitmap.length
+for (let index = 0; index < bitmapLength; index++) {
+
+    const bin: string = bitmap[index].charCodeAt(0).toString(2)
+    if (bin !== "11111111") {
+        const row: string = `${index} ${bitmap[index]} ${addLeadingZeros(bin)}`
+        console.log(row);
+    }
+    // fs.appendFileSync("./outputs/binOut.txt", addLeadingZeros(bin) + "\n", "binary")
+}
+
+// const fileRows = fs.readFileSync("./outputs/binOut.txt", "binary").split("\n")
+
+// fileRows.forEach((row, index) => {
+//     const [base, middle, topLeft, bottomRight] = row.split("  ")
+//     if (base !== bottomRight) {
+//         console.log(index, row);
+//     }
+// })
